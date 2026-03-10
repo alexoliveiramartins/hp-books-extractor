ade8733fefe1e4ab3527fe0514c3d704db628dad
Alex Martins <alex.oliveiramartins05@gmail.com>
2026-03-10 20:59:18 -0300

    Finish chunking script with chapter, book, and text for paragraphs

--- chunker.py
+++ chunker.py
@@ -1,18 +1,42 @@
 import json
 import os
 import sys
+import json
 
-book_path = './data/book.txt'
+markdown_path = './output/output.md'
 
-if not (os.path.isfile(book_path)):
+if not (os.path.isfile(markdown_path)):
     print("Txt file does not exist")
     sys.exit()
 
-with open(book_path, 'r') as book_file:
-    raw_book = book_file.read()
-    book_chunks = raw_book.split('\n\n')
+with open(markdown_path, 'r') as md_file:
+    raw_book = md_file.read()
+    book_chunks = raw_book.split('\n')
 
-for chunk in book_chunks:
-    print(chunk + "\n")
+with open("./output/chunks.jsonl", "w") as file:
+    current_chapter = 0
+    current_book = -1           # -1 por que tem um "CONTENTS" pro conteudo da colecao
+    for chunk in book_chunks:
+        if(chunk == '' or chunk.lower().startswith('**==> picture')):
+            continue
+        if(chunk.startswith("## CONTENTS")):
+            # print(f'Book {current_book} chapters: {current_chapter}')
+            current_book += 1
+            current_chapter = 0
+        if(
+            chunk.startswith('**CHAPTER')
+         or chunk.startswith("## **CHAPTER")
+         or chunk.startswith('CHAPTER')
+         ):
+            current_chapter += 1
+        chunk_json = (
+            {
+                "book": current_book,
+                "chapter": current_chapter,
+                "text": chunk
+            }
+        )
+        file.write(json.dumps(chunk_json, ensure_ascii=False) + "\n")
+        # print(chunk)
 
 print(f'Number of chunks: {len(book_chunks)}')
